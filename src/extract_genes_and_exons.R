7b6720726738f126f55daaba2037adde0b744b8a
Shannon Taylor <shannon.elisa.taylor@gmail.com>
2018-04-05 10:14:18 +1200

    Update extract_genes_and_exons.R

--- src/extract_genes_and_exons.R
+++ src/extract_genes_and_exons.R
@@ -26,20 +26,20 @@ gff_subset <- gff3[seqnames(gff3) %in% c("Group1.4", "Group3.5")]
 #gff_subset <- gff3
 
 # make a table of genes and exons
-genes <- gff_subset[gff_subset$type == "gene"]
+genes <- gff_subset[gff_subset$type == "mRNA"] #mRNA not gene because that's how the Amel2.0 annotation is formatted
 gene_data <- data.table(as.character(seqnames(genes)),
            start(genes),
            end(genes),
            as.character(strand(genes)),
-           genes$ID)
+           genes$ID) #check the genes are denoted with ID- may need to manually edit gff
 
-exons <- gff_subset[gff_subset$type == "exon"]
+exons <- gff_subset[gff_subset$type == "CDS"] #CDS not gene
 exon_data <- data.table(
     as.character(seqnames(exons)),
     start(exons),
     end(exons),
     as.character(strand(exons)),
-    Parent = unlist(exons$Parent))
+    Parent = unlist(exons$ID))
 
 # name the exons
 exon_data[, rank := 1:length(V1), by = Parent]
